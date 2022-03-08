78df4cdd4e4722f35039fe389dd90d28f9ca9680
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2022-03-08 20:17:34 +0100

    💚 ci: investigate html=None

--- build.py
+++ build.py
@@ -220,6 +220,7 @@ for elt, path in pandoc.iter(notebook_doc, path=True):
             parser = lxml.etree.HTMLParser()
             tree = lxml.etree.parse(io.StringIO(content), parser)
             html = tree.getroot()
+            print(f"{tree = }, {html =}")
             if len(html):
                 videos = list(html.iter("video"))
                 if videos:
